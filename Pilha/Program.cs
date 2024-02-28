ea70309fe8d677ab9090bc4ddf30d6920049f928
JoaoRobert0 <chavescamboim2004@gmail.com>
2024-02-27 21:23:19 -0300

    Atualizando a Pilha para testes

--- Pilha/Program.cs
+++ Pilha/Program.cs
@@ -1,65 +1,105 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Bem-vindo ao TAD Pilha");
-        Console.WriteLine("Antes de testar o programa,\ninforme um valor inicial para a capacidade da pilha");
-        Console.Write("Capacidade = ");
-
-        int x = int.Parse(Console.ReadLine());
-        if (x <= 0) throw new Exception("Valor invalido!");
-
-        Pilha p = new Pilha(x);
-
-        Console.Clear();
-        while (true)
+        Console.WriteLine("1-Calcular tempo com grandes entradas\n2-Visualizar pilha");
+        Console.Write("Selecione uma opção: ");
+        int escolha = int.Parse(Console.ReadLine());
+        if (escolha == 1)
         {
-            Console.WriteLine("1-Push\n2-Pop\n3-Show\n4-Size\n5-Empty\n6-Top\n7-Capacity\n8-Clear");
-            Console.Write("Selecione uma opção: ");
-            try{
-                x = int.Parse(Console.ReadLine());
-            }catch (Exception ex){
-                Console.WriteLine("Digite novamente, um numero entre as opções");
-            }
-            switch (x)
+            Pilha p = new Pilha(1000000);
+
+            Stopwatch stopWatch = new Stopwatch();
+            
+            // Nome do arquivo que sera lido
+            string caminhoArquivo = @"..\Testes\teste_100000_1.dat";
+
+            if (File.Exists(caminhoArquivo))
             {
-                case 1:
-                    Console.WriteLine("Digite um objeto a ser armazenado: ");
-                    object o = Console.ReadLine();
-                    p.Push(o);
-                    Console.WriteLine();
-                    break;
-                case 2:
-                    Console.WriteLine($"Objeto removido foi: {p.Pop()}\n");
-                    break;
-                case 3:
-                    p.Show();
-                    Console.WriteLine();
-                    break;
-                case 4:
-                    Console.WriteLine(p.Size());
-                    Console.WriteLine();
-                    break;
-                case 5:
-                    Console.WriteLine(p.IsEmpty());
-                    Console.WriteLine();
-                    break;
-                case 6:
-                    Console.WriteLine(p.Top());
-                    Console.WriteLine();
-                    break;
-                case 7:
-                    Console.WriteLine(p.Capacity());
-                    Console.WriteLine();
-                    break;
-                case 8:
-                    Console.Clear();
-                    break;
+                stopWatch.Start();
+
+                using (StreamReader sr = new StreamReader(caminhoArquivo))
+                {
+                    string linha;
+                    while ((linha = sr.ReadLine()) != null)
+                    {
+                        int valor = int.Parse(linha);
+                        p.Push(valor);
+                    }
+                }
+                stopWatch.Stop();
+                TimeSpan tempoDecorrido = stopWatch.Elapsed;
+                
+                Console.WriteLine(tempoDecorrido);
+            }
+            else
+            {
+                Console.WriteLine("Arquivo não existente");
+            }
+            
+        }
+        else
+        {
+            Console.WriteLine("Bem-vindo ao TAD Pilha");
+            Console.WriteLine("Antes de testar o programa,\ninforme um valor inicial para a capacidade da pilha");
+            Console.Write("Capacidade = ");
+
+            int x = int.Parse(Console.ReadLine());
+            if (x <= 0) throw new Exception("Valor invalido!");
+
+            Pilha p = new Pilha(x);
+
+            Console.Clear();
+            while (true)
+            {
+                Console.WriteLine("1-Push\n2-Pop\n3-Show\n4-Size\n5-Empty\n6-Top\n7-Capacity\n8-Clear");
+                Console.Write("Selecione uma opção: ");
+                try{
+                    x = int.Parse(Console.ReadLine());
+                }catch (Exception ex){
+                    Console.WriteLine("Digite novamente, um numero entre as opções");
+                }
+                switch (x)
+                {
+                    case 1:
+                        Console.WriteLine("Digite um objeto a ser armazenado: ");
+                        object o = Console.ReadLine();
+                        p.Push(o);
+                        Console.WriteLine();
+                        break;
+                    case 2:
+                        Console.WriteLine($"Objeto removido foi: {p.Pop()}\n");
+                        break;
+                    case 3:
+                        p.Show();
+                        Console.WriteLine();
+                        break;
+                    case 4:
+                        Console.WriteLine(p.Size());
+                        Console.WriteLine();
+                        break;
+                    case 5:
+                        Console.WriteLine(p.IsEmpty());
+                        Console.WriteLine();
+                        break;
+                    case 6:
+                        Console.WriteLine(p.Top());
+                        Console.WriteLine();
+                        break;
+                    case 7:
+                        Console.WriteLine(p.Capacity());
+                        Console.WriteLine();
+                        break;
+                    case 8:
+                        Console.Clear();
+                        break;
+                }
             }
         }
-
     }
 }
 
@@ -97,6 +137,7 @@ class Pilha
         if (topo == capacide - 1)
         {
             // Estrategia de duplicação
+            // Testar as outras estrategias
             capacide *= 2;
             object[] arrayAux = new object[capacide];
             
