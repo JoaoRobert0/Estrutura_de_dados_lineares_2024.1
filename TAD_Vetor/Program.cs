22205a19a102261f5ddcb45885b582d301c6aa2e
João Roberto Chaves Camboim <20231014040011@ifrn.local>
2024-04-03 10:29:10 -0300

    Starting Vetor

---
+++ TAD_Vetor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+    }
+}
+
+class Vetor
+{
+    private object[] array;
+    private int capacity;
+    private int size;
+    public Vetor()
+    {
+        capacity = 2;
+        array = new object[capacity];
+        size = 0;
+    }
+    public object ElementAtRank(int r)
+    {
+        
+    }
+    public object ReplaceAtRank(int r, object o)
+    {
+
+    }
+    public void InsertAtRank(int r, object o)
+    {
+        if (r >= capacity) throw new Exception("Indice invalido!");
+    }
+    public object RemoveAtRank(int r)
+    {
+
+    }
+    public int Size() { return size; }
+    public bool IsEmpty()
+    {
+        if (Size() == 0) return true;
+        return false;
+    }
+}
